f77e1bf9141971fc780f39008c097a480bff2600
Monageng <motsabi.monageng@multiply.co.za>
2023-12-27 10:23:55 +0200

    intial

---
+++ src/main/java/liger/co/za/backendservice/LigerBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package liger.co.za.backendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigerBackendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LigerBackendServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/liger/co/za/backendservice/LigerBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package liger.co.za.backendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigerBackendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
